932bd5e22b98d1fa52c1325331b0b516a0789e32
Bhartiya_Hacker <108232080+Bhartiya-Hacker@users.noreply.github.com>
2022-12-28 13:13:23 +0530

    Add files via upload

---
+++ metsploit.sh
@@ -0,0 +1,99 @@
+#!/data/data/com.termux/files/usr/bin/bash
+clear
+echo "
+    +-+-+-+-+-+-+-+-+-+-+ +-+-+ +-+-+-+-+-+-+
+    |M|e|t|a|s|p|l|o|i|t| |i|n| |T|e|r|m|u|x|
+    +-+-+-+-+-+-+-+-+-+-+ +-+-+ +-+-+-+-+-+-+
+            +-+-+ +-+-+-+-+-+-+-+-+-+-+
+              |b|y| |BHARTIYA HACKER|
+            +-+-+ +-+-+-+-+-+-+-+-+-+-+
+"
+
+center() {
+  termwidth=$(stty size | cut -d" " -f2)
+  padding="$(printf '%0.1s' ={1..500})"
+  printf '%*.*s %s %*.*s\n' 0 "$(((termwidth-2-${#1})/2))" "$padding" "$1" 0 "$(((termwidth-1-${#1})/2))" "$padding"
+}
+
+# Loading spinner
+center " Loading..."
+source <(echo "c3Bpbm5lcj0oICd8JyAnLycgJy0nICdcJyApOwoKY291bnQoKXsKICBzcGluICYKICBwaWQ9JCEKICBmb3IgaSBpbiBgc2VxIDEgMTBgCiAgZG8KICAgIHNsZWVwIDE7CiAgZG9uZQoKICBraWxsICRwaWQgIAp9CgpzcGluKCl7CiAgd2hpbGUgWyAxIF0KICBkbyAKICAgIGZvciBpIGluICR7c3Bpbm5lcltAXX07IAogICAgZG8gCiAgICAgIGVjaG8gLW5lICJcciRpIjsKICAgICAgc2xlZXAgMC4yOwogICAgZG9uZTsKICBkb25lCn0KCmNvdW50" | base64 -d)
+
+echo
+center "*** Dependencies installation..."
+
+# Purge installed ruby
+apt purge ruby -y
+rm -fr $PREFIX/lib/ruby/gems
+
+pkg upgrade -y -o Dpkg::Options::="--force-confnew"
+
+# needs binutils
+pkg install -y binutils python autoconf bison clang coreutils curl findutils apr apr-util postgresql openssl readline libffi libgmp libpcap libsqlite libgrpc libtool libxml2 libxslt ncurses make ncurses-utils ncurses git wget unzip zip tar termux-tools termux-elf-cleaner pkg-config git ruby -o Dpkg::Options::="--force-confnew"
+
+python3 -m pip install --upgrade pip
+python3 -m pip install requests
+
+
+# if any weird warning occurs maybe its becoze of bigdecimal & pg_ext.so . try comment those lines if problem persist
+
+echo
+center "*** Fix ruby BigDecimal"
+source <(curl -sL https://github.com/termux/termux-packages/files/2912002/fix-ruby-bigdecimal.sh.txt)
+
+echo
+center "*** Erasing old metasploit folder..."
+rm -rf $PREFIX/opt/metasploit-framework
+
+echo
+center "*** Downloading..."
+cd $PREFIX/opt
+git clone https://github.com/rapid7/metasploit-framework.git --depth=1
+
+echo
+center "*** Installation..."
+cd $PREFIX/opt/metasploit-framework
+
+gem install bundler
+declare NOKOGIRI_VERSION=$(cat Gemfile.lock | grep -i nokogiri | sed 's/nokogiri [\(\)]/(/g' | cut -d ' ' -f 5 | grep -oP "(.).[[:digit:]][\w+]?[.].")
+
+gem install nokogiri -v $NOKOGIRI_VERSION -- --use-system-libraries
+
+bundle config build.nokogiri "--use-system-libraries --with-xml2-include=$PREFIX/include/libxml2"; bundle install
+
+gem install actionpack
+bundle update activesupport
+bundle update --bundler
+bundle install -j$(nproc --all)
+
+if [ -e $PREFIX/bin/msfconsole ];then
+	rm $PREFIX/bin/msfconsole
+fi
+if [ -e $PREFIX/bin/msfvenom ];then
+	rm $PREFIX/bin/msfvenom
+fi
+if [ -e $PREFIX/bin/msfrpcd ];then
+	rm $PREFIX/bin/msfrpcd
+fi
+ln -s $PREFIX/opt/metasploit-framework/msfconsole $PREFIX/bin/
+ln -s $PREFIX/opt/metasploit-framework/msfvenom $PREFIX/bin/
+ln -s $PREFIX/opt/metasploit-framework/msfrpcd $PREFIX/bin/
+
+termux-elf-cleaner $PREFIX/lib/ruby/gems/*/gems/pg-*/lib/pg_ext.so
+
+echo
+center "*"
+echo -e "\033[32m Suppressing Warnings\033[0m"
+
+# Warning occurs during payload generation
+sed -i '86 {s/^/#/};96 {s/^/#/}' $PREFIX/lib/ruby/gems/3.1.0/gems/concurrent-ruby-1.0.5/lib/concurrent/atomic/ruby_thread_local_var.rb
+sed -i '442, 476 {s/^/#/};436, 438 {s/^/#/}' $PREFIX/lib/ruby/gems/3.1.0/gems/logging-2.3.1/lib/logging/diagnostic_context.rb
+
+## openssl issue has been fixed 
+
+cd $HOME && rm metasploit.sh
+
+echo
+center "*"
+echo -e "\033[32m Installation complete. \n Launch metasploit by executing: msfconsole\033[0m"
+center "*"
